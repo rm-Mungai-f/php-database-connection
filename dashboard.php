1cb7cd20b2ecb78036195aefc3c4c8134d446098
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2023-05-18 13:11:35 +0300

    add profile functionality to dashboard

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,54 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Welcome to the dashboard</h1>
+    <div>
+        <nav>
+            <div>
+                <ul>
+                    <li><a href="dashboard.php?id=profile">Profile</a></li>
+                    <li><a href="dashboard.php?id=statements">View statements</a></li>
+                    <li><a href="dashboard.php?id=loans">Apply loans</a></li>
+                    <li><a href="dashboard.php?id=reports">Reports</a></li>
+                    <li><a href="dashboard.php?id=logout">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </body>
 </html>
+
+<?php
+if (isset($_GET['id'])){
+    $selected = $_GET['id'];
+    switch ($selected) {
+        case 'profile':
+            include_once "profile.php";
+            break;
+
+        case 'statements':
+            echo "You selected View statements ";
+            break;
+
+        case 'statements':
+            echo "You selected View statements ";
+            break;
+
+        case 'loans':
+            echo "You selected Apply loans ";
+            break;
+
+        case 'reports':
+            echo "You selected reports ";
+            break;
+
+        case 'logout':
+            echo "You selected logout ";
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+}
+?>
+
---
+++ profile.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table{
+            width: 100%;
+        }
+        table, tr, th, td{
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <?php
+       echo "<table>
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Confirm password</th>
+                <th colspan='2'>Action</th>
+            </tr>";
+             include_once "config.php";
+             $sql = "SELECT * FROM users WHERE fname='munga'";
+             $result = $database_connection->query($sql);
+             $row = $result-> fetch_assoc();
+             echo "<tr>
+                        <td>" . $row['fname']. "</td>
+                        <td>" . $row['lname']. "</td>
+                        <td>" . $row['email']. "</td>
+                        <td>" . $row['password']. "</td>
+                        <td>" . $row['confirm_password']. "</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                    </tr>";
+        echo "</table>";
+        ?>
+    </div>
+</body>
+</html>
